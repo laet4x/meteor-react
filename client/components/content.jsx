4c0cc84e56ad2d9b45e5d47cd6b8750c044ac210
faavictoriano <victorianofrancis294@gmail.com>
2017-02-06 17:03:39 +0800

    remove unused package

--- client/components/content.jsx
+++ client/components/content.jsx
@@ -1,8 +1,14 @@
+//import React 
 import React, { Component } from 'react';
 
+//import meteor
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 
+//import collection
+import Todos from '/imports/api/collection.js';
+
+//import materialUI
 import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
@@ -32,14 +38,14 @@ class Content extends Component {
 
 
   render() {
-    const { user } = this.props;
-    console.log(user);
+    const { todos } = this.props;
+    console.log(todos);
     return (
       <div>
           <h1>Hello, {this.props.name}</h1>
           <ul> 
-             {user.map((user) =>
-                <li>{user.username}</li>
+             {todos.map((todos) =>
+                <li key={todos._id}>{user.name}</li>
               )}
           </ul>
       </div>
@@ -52,6 +58,6 @@ export default Content = createContainer(props => {
   // props here will have `main`, passed from the router
   // anything we return from this function will be *added* to it
   return {
-    user: Meteor.users.find().fetch(),
+    todos: Todos.find({}).fetch(),
   };
 }, Content);
--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
-import { Todos } from '/imports/api/collection.js';
+import Todos from '/imports/api/collection.js';
 mysql  = Npm.require("mysql");
 Meteor.startup(() => {
  console.log('server running....');
+ //Todos.insert({_id:"1",name:"frankie"});
 });
